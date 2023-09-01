973a76475ba40fab928c8abc218335b3e479ea08
Will Killian <william.killian@outlook.com>
2023-09-01 19:19:12 -0400

    Fix varying length issue
    Signed-off-by: Will Killian <william.killian@outlook.com>

--- include/named-tuple/named-tuple.hpp
+++ include/named-tuple/named-tuple.hpp
@@ -29,33 +29,34 @@ struct fixed_string : public std::array<char, N - 1>
 
   template<size_t M>
   [[nodiscard]] constexpr bool
-  operator==(fixed_string<M> const&) noexcept
-    requires(M != N)
+  eq(fixed_string<M> const& o) const noexcept
   {
-    return false;
+    return this->size() == o.size() &&
+           std::equal(this->begin(), this->end(), o.begin(), o.end());
   }
 
   template<size_t M>
   [[nodiscard]] constexpr bool
-  operator!=(fixed_string<M> const&) noexcept
-    requires(M != N)
+  neq(fixed_string<M> const& o) const noexcept
   {
-    return true;
+    return !eq(o);
   }
 
-  explicit operator std::string_view () const noexcept {
+  explicit
+  operator std::string_view() const noexcept
+  {
     return std::string_view{this->data(), this->size()};
   }
 };
 
 template<typename NamedTuple>
-constexpr static bool keys_unique = [] () constexpr {
+constexpr static bool keys_unique = []() constexpr {
   using T = std::remove_reference_t<NamedTuple>;
   constexpr size_t const N{std::tuple_size_v<typename T::type>};
   return []<size_t... Is, size_t... Js>(std::index_sequence<Is...>,
                                         std::index_sequence<Js...>) {
     return (... && []<size_t I>(std::integral_constant<size_t, I>) {
-      return (... && (Js <= I || get<Js>(T::names) != get<I>(T::names)));
+      return (... && (Js <= I || get<Js>(T::names).neq(get<I>(T::names))));
     }(std::integral_constant<size_t, Is>{}));
   }(std::make_index_sequence<N>{}, std::make_index_sequence<N>{});
 }();
@@ -64,7 +65,7 @@ template<typename NamedTuple, fixed_string Key>
 constexpr static bool keys_contain = [] {
   using T = std::remove_reference_t<NamedTuple>;
   return []<size_t... Is>(std::index_sequence<Is...>) {
-    return (... || (Key == get<Is>(T::names)));
+    return (... || (Key.eq(get<Is>(T::names))));
   }(std::make_index_sequence<std::tuple_size_v<typename T::type>>{});
 }();
 
@@ -73,7 +74,7 @@ constexpr static size_t named_index_of = [] {
   using T = std::remove_reference_t<NamedTuple>;
   return []<size_t... Is>(std::index_sequence<Is...>) {
     // yield the index when the key matches (else zero)
-    return (... + ((Key == get<Is>(T::names)) ? Is : 0lu));
+    return (... + ((Key.eq(get<Is>(T::names))) ? Is : 0lu));
   }(std::make_index_sequence<std::tuple_size_v<typename T::type>>{});
 }();
 
@@ -89,8 +90,8 @@ struct named_val
 template<typename... Vs>
 struct named_tuple : public std::tuple<typename Vs::type...>
 {
-  constexpr static const std::tuple names{Vs::name...};
   using type = std::tuple<typename Vs::type...>;
+  constexpr static std::tuple names{Vs::name...};
   static_assert(detail::keys_unique<named_tuple>, "keys must be unique");
   using type::type;
 };
